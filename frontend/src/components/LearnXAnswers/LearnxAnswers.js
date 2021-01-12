eb402de328672438eeec56d4699cae6592052bd1
ThreeOne31 <mahomedkhomo@gmail.com>
2021-01-12 14:48:52 +0200

    added reply section

--- frontend/src/components/LearnXAnswers/LearnxAnswers.js
+++ frontend/src/components/LearnXAnswers/LearnxAnswers.js
@@ -3,37 +3,48 @@ import React from "react"
 import "./style.css"
 
 class LearnxAnswers extends React.Component{
+    
     replyAccordion = (event) =>{
-        let replies = event.target.parentElement.parentElement.parentElement.lastChild
-        let panel = event.target.parentElement.nextElementSibling
-
-        if (panel.style.display === "block") {
-            panel.style.display = "none"
-            replies.style.display = "block"
-        } else {
-            panel.style.display = "block";
-            replies.style.display = "none"
-        }
-    }
-    accordion = (event) =>{
-        console.log(event.target.nextElementSibling)
-        let replies = event.target.nextElementSibling
+        
+        let replies =event.target.parentElement.parentElement.lastChild
 
         if (replies.style.display === "block") {
-            replies.style.display = "none"
-            event.target.style.display = "block"
+            replies.style.display = "none"  
+            event.target.parentElement.parentElement.parentElement.lastChild.style.display = "block"          
         } else {
             replies.style.display = "block";
-            event.target.style.display = "none"
-        }
+            event.target.parentElement.parentElement.parentElement.lastChild.style.display = "none"
+        }  
+    }
+    
+    accordion = (event) =>{
+        event.target.style.display = "none"
+        let questionChildren = event.target.parentElement.children
+        let replies = questionChildren[5].lastChild
+
+        if (replies.style.display === "block") {
+            replies.style.display = "none"            
+        } else {
+            replies.style.display = "block";
+        }          
     }
     render(){
         return(
             <div className="main">
                 <div className="QnA-main-container">
                     <h2>LearnX Answers</h2>
-                    <header>great sort filter search</header>
+                    <div className="queation-header">
+                        <button className="add-question">Add</button>
+                        <select className="question-sort">
+                            <option>Most Recent</option>
+                            <option>Oldest</option>
+                            <option>A &#8594; Z</option>
+                            <option>Z &#8594; A</option>
+                        </select>
+                        <input className="question-search" type="text" name="search" placeholder="Search..." />                        
+                    </div>
                     <div className="question-container">
+                          
                         <div className="question">
                             <span className="time-right">11:00</span>
                             <img src="/assets/images/bandmember.jpg" alt="Avatar" />
@@ -54,46 +65,7 @@ class LearnxAnswers extends React.Component{
                                             <div className="reply-form-container">
                                                 <img src="/assets/images/bandmember.jpg" alt="Avatar" />
                                                 <form>
-                                                    <textarea name="reply-message" placeholder="type reply"/>
-                                                    <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
-                                                </form>                          
-                                            </div>                                            
-                                        </li>
-                                        <li>
-                                            <div className="reply-item">
-                                                <span className="time-right">11:00</span>
-                                                <img src="/assets/images/bandmember.jpg" alt="Avatar" />
-                                                <h3>UserName</h3>
-                                                <p className="reply-message">Hello. How are you today?</p>                            
-                                            </div>
-                                        </li>
-                                    </ul>                
-                            </div>
-                            <div className="reply-count">
-                                <i onClick={this.accordion} className="fa fa-envelope replies-icon"> 4 replies</i>
-                            </div>                                
-                        </div>   
-                        <div className="question">
-                            <span className="time-right">11:00</span>
-                            <img src="/assets/images/bandmember.jpg" alt="Avatar" />
-                            <h3>UserName</h3>
-                            <h4>Question title</h4>
-                            <p>
-                                Whenever I conteplate the blue colour I remember St Boniface, I say why me, why not mosuwe why not lehakoe la pelo eaka
-                                Whenever I conteplate the blue colour I remember St Boniface, I say why me, why not mosuwe why not lehakoe la pelo eaka
-                            </p>
-                            <div className="question-footer">                                
-                                <div>
-                                    <button onClick={this.replyAccordion} className="question-buttons reply">Reply</button>
-                                    <button className="question-buttons follow">follow</button> 
-                                    <span className="question-tags">#physics  #circularMotion</span>
-                                </div>
-                                    <ul className="reply-n-replies">
-                                        <li>
-                                            <div className="reply-form-container">
-                                                <img src="/assets/images/bandmember.jpg" alt="Avatar" />
-                                                <form>
-                                                    <textarea name="reply-message" placeholder="type reply"/>
+                                                    <input type="text" name="reply-message" placeholder="type reply"/>
                                                     <button className="send-reply">send</button>
                                                 </form>                          
                                             </div>                                            
@@ -107,16 +79,13 @@ class LearnxAnswers extends React.Component{
                                             </div>
                                         </li>
                                     </ul>                
-                            </div>
-                            <div className="reply-count">
-                                <i onClick={this.accordion} className="fa fa-envelope replies-icon"> 4 replies</i>
-                            </div>                                
+                            </div>                        
+                            <i onClick={this.accordion} className="fa fa-envelope replies-icon"> 4 replies</i>                                                        
                         </div>                       
                     </div>
                 </div>
             </div>
-        )
-    }
+    )}
 }
 export default LearnxAnswers
 
